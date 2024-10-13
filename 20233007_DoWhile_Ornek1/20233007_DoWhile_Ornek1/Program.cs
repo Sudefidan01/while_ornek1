db3bf071dbea72b13e42722cd5e9cb422c9bc9ad
sude <sudefidan@icloud.com>
2024-10-13 17:18:55 +0300

    while_ornek1

---
+++ 20233007_DoWhile_Ornek1/20233007_DoWhile_Ornek1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20233007_DoWhile_Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName = "ahmet", password = "123456";
+
+            //Kullanıcı adı ve şifre doğru girildikten sonra giriş başarılı denecek
+            //Eğer yanlış giriş yapılırsa 'yanlış kullanıcı adı veya şifre' uyarısı verilecek
+            bool girisYapildiMi=false;
+            do
+            {
+                Console.Write("Kullanıcı Adı : ");
+                string kAdi = Console.ReadLine();
+                Console.Write("Şifre : ");
+                string sifre=Console.ReadLine();
+
+                if (userName==kAdi&&password==sifre)
+                {
+                    girisYapildiMi = true;
+                }
+                if (!girisYapildiMi)
+                {
+                    Console.WriteLine("Yanlış kullanıcı adı veya şifre");
+                }
+
+            } while (!girisYapildiMi);
+            Console.WriteLine("Giriş İşlemi Başarılı");
+            Console.ReadKey();
+        }
+    }
+}
